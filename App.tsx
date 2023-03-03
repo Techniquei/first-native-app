2b4f63de0798795c2234b37c27ae96334493755b
Techniquei <66029763+Techniquei@users.noreply.github.com>
2023-03-03 23:20:03 +0300

    create app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native'
+
+const Post = styled.View`
+  padding: 15px;
+  background-color: black;
+  height: 100px;
+  width:  100px;
+  border-radius: 10px;
+`
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Post />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/assets/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function HomeScreen(){
+    return(
+        <>
+            HOMESCREEN
+            <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" alt="" />
+        </>
+    )
+}
